70dbfc2f07b778c974aebe06d6883b202c592d39
realtedpark <theohpark@gmail.com>
2023-04-03 01:10:44 -0400

    Updated website

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 /*!
-* Start Bootstrap - One Page Wonder v6.0.6 (https://startbootstrap.com/theme/one-page-wonder)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
-*/
+ * Start Bootstrap - One Page Wonder v6.0.6 (https://startbootstrap.com/theme/one-page-wonder)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
+ */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
 
+
+// With assistance from ChatGPT
+// This code reads from a hosted json file and feeds the information into Bootstrap cards
+// I learned how to read from a json file after hours wasted trying stuff out on StackOverflow
 
 // Create a new XMLHttpRequest object
 const xhr = new XMLHttpRequest();
 
 // Set the URL of the JSON file you want to load
-const url = "https://jsonplaceholder.typicode.com/posts";
+const url = "https://realtedpark.github.io/website-challenge/menu.json";
 
 // Set the request method to "GET"
 xhr.open("GET", url);
@@ -24,46 +28,42 @@ xhr.send();
 
 // Handle the response when it arrives
 xhr.onload = () => {
-  // Get the JSON data from the response
-  const jsonData = xhr.response;
+    // Get the JSON data from the response
+    const juices = xhr.response.juices;
+    const bowls = xhr.response.bowls;
+    const snacks = xhr.response.snacks;
 
-  // Use the JSON data here
-  console.log(jsonData);
+    var juiceMenu = document.getElementById("juices");
+    for (var i = 0; i < juices.length; i++) {
+        console.log(juices[i].name);
+        var card = document.createElement("div");
+
+        card.className = "card h-100 border-0";
+        card.style = "width: 18rem;";
+        card.innerHTML = "<img class=\"card-img-top\" src=" + juices[i].img + " alt=" + juices[i].name + " /><div class=\"card-body\"><h5 class=\"card-title\">" + juices[i].name + "</h5><p class=\"card-text\">" + juices[i].desc + "</p></div>";
+        juiceMenu.appendChild(card);
+    }
+
+    var bowlMenu = document.getElementById("bowls");
+    for (var i = 0; i < bowls.length; i++) {
+        console.log(bowls[i].name);
+        var card = document.createElement("div");
+
+        card.className = "card h-100 border-0";
+        card.style = "width: 18rem;";
+        card.innerHTML = "<img class=\"card-img-top\" src=" + bowls[i].img + " alt=" + bowls[i].name + " /><div class=\"card-body\"><h5 class=\"card-title\">" + bowls[i].name + "</h5><p class=\"card-text\">" + bowls[i].desc + "</p></div>";
+        bowlMenu.appendChild(card);
+    }
+
+    var snackMenu = document.getElementById("snacks");
+    for (var i = 0; i < snacks.length; i++) {
+        console.log(snacks[i].name);
+        var card = document.createElement("div");
+
+        card.className = "card h-100 border-0";
+        card.style = "width: 18rem;";
+        card.innerHTML = "<img class=\"card-img-top\" src=" + snacks[i].img + " alt=" + snacks[i].name + " /><div class=\"card-body\"><h5 class=\"card-title\">" + snacks[i].name + "</h5><p class=\"card-text\">" + snacks[i].desc + "</p></div>";
+        snackMenu.appendChild(card);
+    }
+
 };
-
-
-// var juiceMenu = document.getElementById("test1");
-// var juices = ('../menu.json').juices;
-// for (var i = 0; i < juices.length; i++) {
-//     console.log("test");
-//     console.log(juices[i].name);
-//     var card = document.createElement("p");
-//     card.innerHTML = juices[i].name;
-//     juiceMenu.appendChild(card);
-// }
-
-// document.getElementById(function() {
-//     //hide first div or remove after append using `$(".card:first").remove()`
-//     console.log("test1");
-//     $(".card:first").hide();
-//     $.ajax({
-//       url: "https://jsonplaceholder.typicode.com/posts",
-//       success: function(result) {
-//         $.each(result, function(index, item) {
-//           var cards = $(".card:first").clone() //clone first divs
-//           var userId = item.userId;
-//           var typeId = item.id;
-//           var titleId = item.title;
-//           var bodyId = item.body;
-//           //add values inside divs
-//           $(cards).find(".card-header").html("user id: " + userId + " - " + "id: " + typeId);
-//           $(cards).find(".card-title").html(titleId);
-//           $(cards).find(".card-text").html(bodyId);
-//           $(cards).show(); //show cards
-//           $(cards).appendTo($(".test1")); //append to container
-//         });
-//       }
-//     });
-//   });
-
-  console.log("test2");
